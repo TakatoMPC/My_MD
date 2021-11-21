b21c63f812e429cb183f74a8bf5ebc3b4d7de8e5
takato <takato.matphysichem@gmail.com>
2021-11-22 01:34:16 +0900

    folders re-organized

---
+++ MDr/.ipynb_checkpoints/analysis-checkpoint.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/takato/Simulation/MD/Temperatures.csv', names=['Ta', 'Tb'], header = 1)
+
+MDstep = range(10000)
+Ta = df.loc[:, "Ta"]
+Tb = df.loc[:, "Tb"]
+
+Tb.head()
+
+
+plt.plot(MDstep, Tb) 
+plt.show()
